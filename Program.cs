c2e95dba0aba0291c96b088bba7014e9d69d3340
Hevellyn <celularhduarte@gmail.com>
2025-09-22 18:21:38 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaném");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Iguape");
+
+
+
